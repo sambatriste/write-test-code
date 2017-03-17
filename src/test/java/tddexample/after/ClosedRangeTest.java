e9eabf232c19471065ee59c5a5a63d27c06a006d
KAWASAKI Tsuyoshi <kawasaki.tsuyoshi@tis.co.jp>
2017-03-17 16:11:12 +0900

    テスト整備.

--- src/test/java/tddexample/after/ClosedRangeTest.java
+++ src/test/java/tddexample/after/ClosedRangeTest.java
@@ -9,77 +9,6 @@ import org.junit.Test;
 
 public class ClosedRangeTest {
 
-
-    @Test
-    public void 下端点と上端点を指定してnewできる() {
-        ClosedRange c3to8 = new ClosedRangeTest.ClosedRange(3, 8);
-        assertNotNull(c3to8);
-    }
-
-    @Test
-    public void 下端点を取得できる() {
-
-        // [3,8] を生成
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        // [3,8] の下端点は 3
-        assertEquals(3, c3to8.getLowerEndpoint());
-    }
-
-    @Test
-    public void 上端点を取得できる() {
-
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-
-        assertEquals(8, c3to8.getUpperEndPoint());
-    }
-
-    //  [3,8] の文字列表記は "[3,8]"
-    @Test
-    public void 文字列表現を取得できる() {
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        assertEquals("[3,8]", c3to8.toString());
-    }
-
-    @Test
-    public void 上端点と下端点が逆転している場合_例外が発生する() {
-        try {
-            ClosedRange c8to3 = new ClosedRange(8, 3); // => エラー
-            fail("例外が発生するはずです！");
-        } catch (IllegalArgumentException e) {
-            return; // 期待した例外が発生した
-        }
-    }
-
-    // [3,8] は 5 を含む
-    @Test
-    public void 閉区間が指定した整数を含む場合_trueを返却() {
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        assertEquals(true, c3to8.contains(5));
-    }
-
-    // [3,8] は -1 を含まない
-    @Test
-    public void 閉区間が指定した整数を含まない場合_falseを返却() {
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        assertEquals(false, c3to8.contains(-1));
-    }
-
-    // [3,8] は [3,8] と等しい
-    @Test
-    public void ふたつの閉区間が等しい場合_trueを返却() {
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        assertEquals(true, c3to8.equals(new ClosedRange(3, 8)));
-    }
-
-
-    @Test
-    public void ふたつの閉区間が等しくない場合_falseを返却() {
-        ClosedRange c3to8 = new ClosedRange(3, 8);
-        ClosedRange c1to6 = new ClosedRange(1, 6);
-        assertEquals(false, c3to8.equals(c1to6)); // => false
-    }
-
-
     static class ClosedRange {
         /** 下端点 */
         private int lower;
@@ -131,7 +60,10 @@ public class ClosedRangeTest {
             return lower <= number && number <= upper;
         }
 
-        /** {@inheritDoc} */
+        /**
+         * {@inheritDoc}
+         * 上端点と下端点ともに等しいとき、等価と判定する。
+         */
         @Override
         public boolean equals(Object obj) {
             if (!(obj instanceof ClosedRange)) {
@@ -140,5 +72,72 @@ public class ClosedRangeTest {
             ClosedRange another = (ClosedRange) obj;
             return another.lower == this.lower && another.upper == this.upper;
         }
+    }
+
+    @Test
+    public void 下端点と上端点を指定してnewできる() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        assertNotNull(c3to8);
+    }
+
+    @Test
+    public void 下端点を取得できる() {
+
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        // [3,8] の下端点は 3
+        assertEquals(3, c3to8.getLowerEndpoint());
+    }
+
+    @Test
+    public void 上端点を取得できる() {
+
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        // [3,8] の上端点は 8
+        assertEquals(8, c3to8.getUpperEndPoint());
+    }
+
+    //  [3,8] の文字列表記は "[3,8]"
+    @Test
+    public void 文字列表現を取得できる() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        assertEquals("[3,8]", c3to8.toString());
+    }
+
+    @Test
+    public void 上端点と下端点が逆転しているとき例外が発生する() {
+        try {
+            ClosedRange c8to3 = new ClosedRange(8, 3); // => エラー
+            fail("例外が発生するはずです！");
+        } catch (IllegalArgumentException e) {
+            return; // 期待通りの例外が発生
+        }
+    }
+
+    // [3,8] は 5 を含む
+    @Test
+    public void 閉区間が指定した整数を含むときtrueが返却される() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        assertEquals(true, c3to8.contains(5));
+    }
+
+    // [3,8] は -1 を含まない
+    @Test
+    public void 閉区間が指定した整数を含まないときfalseが返却される() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        assertEquals(false, c3to8.contains(-1));
+    }
+
+    // [3,8] は [3,8] と等しい
+    @Test
+    public void ふたつの閉区間が等しいときtrueが返却される() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        assertEquals(true, c3to8.equals(new ClosedRange(3, 8)));
+    }
+
+    @Test
+    public void ふたつの閉区間が等しくないときfalseが返却される() {
+        ClosedRange c3to8 = new ClosedRange(3, 8);
+        ClosedRange c1to6 = new ClosedRange(1, 6);
+        assertEquals(false, c3to8.equals(c1to6)); // => false
     }
 }
--- src/test/java/tddexample/before/ClosedRangeTest.java
+++ src/test/java/tddexample/before/ClosedRangeTest.java
@@ -23,4 +23,58 @@ public class ClosedRangeTest {
         ClosedRange c3to8 = new ClosedRange(3, 8);
         assertNotNull(c3to8);
     }
+
+    @Test
+    public void 下端点を取得できる() {
+        fail("まだ実装されていません。");
+        // [3,8] の下端点は 3
+    }
+
+    @Test
+    public void 上端点を取得できる() {
+        fail("まだ実装されていません。");
+        // [3,8] の上端点は 8
+    }
+
+
+    @Test
+    public void 文字列表現を取得できる() {
+        fail("まだ実装されていません。");
+        //  [3,8] の文字列表記は "[3,8]"
+    }
+
+    @Test
+    public void 上端点と下端点が逆転しているとき例外が発生する() {
+        fail("まだ実装されていません。");
+        // [8,3]はエラー
+    }
+
+
+    @Test
+    public void 閉区間が指定した整数を含むときtrueが返却される() {
+        fail("まだ実装されていません。");
+        // [3,8] は 5 を含む
+    }
+
+
+    @Test
+    public void 閉区間が指定した整数を含まないときfalseが返却される() {
+        fail("まだ実装されていません。");
+        // [3,8] は -1 を含まない
+    }
+
+
+    @Test
+    public void ふたつの閉区間が等しいときtrueが返却される() {
+        fail("まだ実装されていません。");
+        // [3,8] は [3,8] と等しい
+    }
+
+
+    @Test
+    public void ふたつの閉区間が等しくないときfalseが返却される() {
+        fail("まだ実装されていません。");
+        // [3,8]と[1,6]は等しくない。
+    }
+
 }
